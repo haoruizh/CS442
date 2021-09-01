d1ef703b834e268943cabc5383a80dfb87476c87
Haorui Zhang <haoruizh@gmail.com>
2021-09-01 00:00:22 -0700

    tar the file

---
+++ PA01/pa01_circles/circle.cpp
@@ -0,0 +1,56 @@
+#include "circle.h"
+#include "n_elem.h"
+#include "poly_line.h"
+#include "wrap_cmath_inclusion.h"
+
+
+void Circle::draw(void)
+{
+    // If the circle has not been tessellated, tessellate it.
+    if (!tessellationPolyLine)
+        tessellate();
+
+    // Now draw it.
+    tessellationPolyLine->render();
+}
+
+
+void Circle::tessellate(void)
+//
+// tessellates the circle
+//
+{
+    //
+    // ASSIGNMENT (PA01)
+    //
+    // Replace the square PolyLine code below with code that uses the
+    // formula you derived to set `tessellationPolyLine` to a PolyLine
+    // representing a polygon with the minimum number of sides to be
+    // within a half-pixel of a circle of the given `radius`.
+    //
+    // PolyLine() copies `vertexPosition`, so if you use dynamic
+    // storage (and you should), remember to clean up after yourself.
+    //
+    // 19 lines in instructor solution (YMMV)
+    //
+
+    const GLfloat PI = 3.1415926f;
+    const GLfloat P = 0.00119;
+    GLfloat D = P/2;
+    int n = ceil(50);
+    Point2 vertexPositions[n];
+    // = {
+    //     Point2(-0.5, -0.5),
+    //     Point2( 0.5, -0.5),
+    //     Point2( 0.5,  0.5),
+    //     Point2(-0.5,  0.5),
+    //     Point2(0.7,0.7),
+    //     Point2(0.6,0.6)
+    // };
+    for (int i =0; i < n; i++)
+    {
+        vertexPositions[i] = Point2(this->radius*cos(2*PI/n*i)+this->center.g.x, this->radius*sin(2*PI/n*i)+this->center.g.y);
+    }
+    tessellationPolyLine = new PolyLine(vertexPositions,
+                                        N_ELEM(vertexPositions), true);
+}
--- pa01_circles/passthru_fragment_shader.glsl
+++
@@ -1,9 +0,0 @@
-#version 330
-
-in vec4 vertexColor;
-out vec4 fragmentColor;
-
-void main(void)
-{
-    fragmentColor = vertexColor;
-}
