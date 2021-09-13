6acf8bb20b69115171b009a093c81e8d68394ccf
Haorui Zhang <haoruizh@gmail.com>
2021-09-12 22:40:18 -0700

    add HW3 source code

---
+++ PA03/pa03_wire_car/mesh.cpp
@@ -0,0 +1,16 @@
+#include "check_gl.h"
+#include "mesh.h"
+
+
+void Mesh::allocateBuffers(void)
+{
+    //
+    // ASSIGNMENT (PA03)
+    //
+    // Do the following:
+    //
+    // - Call glGenBuffers() to create `vertexPositionsBufferId`.
+    //
+}
+
+
